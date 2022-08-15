0b34c953a80daab610e00a5c777b375ab93b04c7
Igor Campos Moreira <igorcampos200@gmail.com>
2022-08-15 19:01:14 -0300

    Requisito 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -19,7 +20,11 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    urls = []
+    for url in selector.css("a.cs-overlay-link::attr(href)").getall():
+        urls.append(url)
+    return urls
 
 
 # Requisito 3
