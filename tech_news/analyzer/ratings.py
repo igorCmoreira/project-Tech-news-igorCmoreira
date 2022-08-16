3beb7b4410d317aeef278cd862fdc8ece001bb12
Igor Campos Moreira <igorcampos200@gmail.com>
2022-08-16 01:56:47 -0300

    Requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,17 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    sort = sorted(news, key=lambda x: x["comments_count"], reverse=True)
+
+    top_news = []
+
+    for n in sort[:5]:
+        top_news.append((n["title"], n["url"]))
+
+    return top_news
 
 
 # Requisito 11
