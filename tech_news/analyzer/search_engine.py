746b6c34f653fe24faf6e878bac8dbb918c5fdd8
Igor Campos Moreira <igorcampos200@gmail.com>
2022-08-16 01:39:18 -0300

    Requisito 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -43,4 +43,11 @@ def search_by_tag(tag):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = search_news({"category": {"$regex": category, "$options": "i"}})
+
+    founded_news = []
+
+    for n in news:
+        founded_news.append((n["title"], n["url"]))
+
+    return founded_news
